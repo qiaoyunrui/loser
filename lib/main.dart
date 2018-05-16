e2254dfb4fec3ea3cb2c87b3a033b714d14585c4
qiaoyunrui <shixi_yunrui@staff.weibo.com>
2018-05-16 22:31:56 +0800

    [Update] make android ios different

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:loser/chat_message.dart';
 
@@ -11,13 +13,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
+
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.green,
+);
+
 class HomeApp extends StatelessWidget {
   build(context) {
     return new MaterialApp(
       title: "loser",
-      theme: new ThemeData(
-        primarySwatch: Colors.green,
-      ),
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
       home: new ChatScreen(),
     );
   }
@@ -30,10 +42,14 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final TextEditingController _textController = new TextEditingController();
   final List<ChatMessage> _message = <ChatMessage>[];
+  bool _isComposing = false;
 
   _handleSubmitted(String text) {
 //    print('消息为 $text');
     _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
     if (text.trim().isEmpty) return;
     ChatMessage message = new ChatMessage(
       text: text,
@@ -60,47 +76,73 @@ class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
                 controller: _textController,
                 onSubmitted: _handleSubmitted,
                 decoration: new InputDecoration.collapsed(hintText: '发送消息'),
+                onChanged: (String text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
               ),
             ),
             new Container(
               margin: new EdgeInsets.symmetric(horizontal: 4.0),
-              child: new IconButton(
-                  icon: new Icon(Icons.send),
-                  onPressed: () => _handleSubmitted(_textController.text)),
-            )
+              child: Theme.of(context).platform == TargetPlatform.iOS
+                  ? new CupertinoButton(
+                      // iOS 平台上的按钮样式
+                      child: new Text(
+                        "发送",
+                      ),
+                      onPressed: _onSend(),
+                    )
+                  : new IconButton(
+                      icon: new Icon(Icons.send),
+                      onPressed: _onSend(),
+                    ),
+            ),
           ],
         ),
       ),
     );
   }
 
+  _onSend() =>
+      _isComposing ? () => _handleSubmitted(_textController.text) : null;
+
   build(context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("死肥仔的日常"),
-      ),
-      body: new Column(
-        children: <Widget>[
-          new Flexible(
-            child: new ListView.builder(
-              itemBuilder: (_, int index) => _message[index],
-              padding: new EdgeInsets.all(8.0),
-              reverse: true,
-              itemCount: _message.length,
-            ),
+        appBar: new AppBar(
+          title: new Text("死肥仔的日常"),
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+          // 设置阴影，ios 没有阴影，Android 上有阴影
+        ),
+        body: new Container(
+          child: new Column(
+            children: <Widget>[
+              new Flexible(
+                child: new ListView.builder(
+                  itemBuilder: (_, int index) => _message[index],
+                  padding: new EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemCount: _message.length,
+                ),
+              ),
+              new Divider(
+                height: 1.0,
+              ),
+              new Container(
+                decoration: new BoxDecoration(
+                  color: Theme.of(context).cardColor,
+                ),
+                child: _buildTextComposer(),
+              )
+            ],
           ),
-          new Divider(
-            height: 1.0,
-          ),
-          new Container(
-            decoration: new BoxDecoration(
-              color: Theme.of(context).cardColor,
-            ),
-            child: _buildTextComposer(),
-          )
-        ],
-      ),
-    );
+          decoration: Theme.of(context).platform == TargetPlatform.iOS
+              ? new BoxDecoration(
+                  border:
+                      new Border(top: new BorderSide(color: Colors.grey[200])))
+              : null,
+        ));
   }
 
   dispose() {
